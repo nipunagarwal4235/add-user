b444a7117255936c4e9472d1e65d92fed69acede
nipunagarwal4235 <nipunagarwal2002@gmail.com>
2021-07-05 19:47:12 +0530

    deploying

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React , {useState} from 'react';
+import AddUser from './Components/Users/AddUser';
+import UsersList from './Components/Users/UsersList';
+
+function App() {
+  const [usersList , setUsersList] = useState ([]);
+
+  const addUserHandler = (uName , uAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList , {name:uName , age: uAge , id:Math.random().toString()} , ];
+    });
+  };
+  return (
+    <div>
+      <AddUser onAddUser = {addUserHandler}></AddUser>
+      <UsersList users={usersList}></UsersList>
+    </div>
+  );
+}
+
+export default App;
